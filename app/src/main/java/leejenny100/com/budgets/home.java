b9d84552d272d9b616416ba4015b6e43e1429497
Jenny <tlee89@illinois.edu>
2018-05-03 13:20:26 -0500

    Added firebase to backend :) and the calculator = fixed increase and decrease added for now

--- app/src/main/java/leejenny100/com/budgets/home.java
+++ app/src/main/java/leejenny100/com/budgets/home.java
@@ -1,24 +1,28 @@
 package leejenny100.com.budgets;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
-import android.app.Activity;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class home extends AppCompatActivity {
-
+    private TextView countertext;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        countertext = findViewById(R.id.editText2);
+
+        TextView date = findViewById(R.id.editText);
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        date.setText(currentDateTimeString);
 
         Button calendarbutton = findViewById(R.id.button9);
         calendarbutton.setOnClickListener(new OnClickListener() {
@@ -35,7 +39,15 @@ public class home extends AppCompatActivity {
                 startActivity(new Intent(home.this, calculator.class));
             }
         });
-
-
     }
-}
+
+        public void dec10 (View view){
+            counter += 10;
+            countertext.setText(String.valueOf(counter));
+        }
+        public void inc10 (View view){
+            counter -= 10;
+            countertext.setText(String.valueOf(counter));
+        }
+    }
+
